c87d1b1051a6d7a92fadd6b5f914fda76662a2db
anniebeva <anniebeva@gmail.com>
2026-07-22 18:50:21 +0300

    Add Selenium driver setup and browser test

---
+++ tests/test_browser.py
@@ -0,0 +1,14 @@
+from utils.driver import get_driver
+
+
+def test_open_saucedemo():
+    """
+    Test browser access
+    """
+
+    driver = get_driver()
+    driver.get("https://www.saucedemo.com/")
+
+    assert "Swag Labs" in driver.title
+
+    driver.quit()
